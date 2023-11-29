bab8a5f846e1e2479b637137bc1d3a57cfc804fe
mtyszczak <mateusz.tyszczak@gmail.com>
2023-11-29 12:16:50 +0100

    Divide worker and queen classes into separate files

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,11 @@
 import EventEmitter from "events";
 import beekeeperFactory, { IBeekeeperInstance, IBeekeeperOptions, IBeekeeperUnlockedWallet } from "@hive-staging/beekeeper";
-import { BroadcastTransactionRequest, calculateExpiration, IWaxOptionsChain, operation, transaction, TWaxExtended } from "@hive-staging/wax";
-import type { Observer, Subscribable, Unsubscribable } from "rxjs";
+import { BroadcastTransactionRequest, calculateExpiration, IWaxOptionsChain, transaction, TWaxExtended } from "@hive-staging/wax";
+import type { Subscribable } from "rxjs";
 
-import { AccountOperationVisitor } from "./account_observer";
 import { WorkerBeeError } from "./errors";
-import type { IWorkerBee, IBlockData, ITransactionData, IOperationData, IBroadcastOptions } from "./interfaces";
+import type { IWorkerBee, IBlockData, ITransactionData, IBroadcastOptions } from "./interfaces";
+import { QueenBee } from "./queen";
 import { getWax, WaxExtendTypes } from "./wax/extend";
 
 const ONE_MINUTE = 1000 * 60;
@@ -40,116 +40,6 @@ export const DEFAULT_WORKERBEE_OPTIONS = {
 };
 
 export const DEFAULT_BLOCK_INTERVAL_TIMEOUT = 1500;
-
-export class QueenBee {
-  public constructor(
-    private readonly worker: WorkerBee
-  ) {}
-
-  public block(idOrNumber: string | number): Subscribable<IBlockData> {
-    return {
-      subscribe: (observer: Partial<Observer<IBlockData>>): Unsubscribable => {
-        const complete = (): void => {
-          try { observer.complete?.(); } catch (error) { try { observer.error?.(error); } catch {} }
-          this.worker.off("block", listener);
-        };
-
-        const listener = (blockData: IBlockData): void => {
-          const confirm = (): void => {
-            try { observer.next?.(blockData); } catch (error) { try { observer.error?.(error); } catch {} }
-            complete();
-          };
-
-          if(typeof idOrNumber === "string") {
-            if(idOrNumber === blockData.block.block_id)
-              confirm();
-          } else if(idOrNumber === blockData.number)
-            confirm();
-        };
-        this.worker.on("block", listener);
-
-        return {
-          unsubscribe: (): void => {
-            complete();
-          }
-        };
-      }
-    };
-  }
-
-  public transaction(txId: string, expireIn?: number): Subscribable<ITransactionData> {
-    return {
-      subscribe: (observer: Partial<Observer<ITransactionData>>): Unsubscribable => {
-        let timeoutId: undefined | NodeJS.Timeout = undefined;
-
-        const complete = (): void => {
-          try { observer.complete?.(); } catch (error) { try { observer.error?.(error); } catch {} }
-          this.worker.off("transaction", listener);
-          clearTimeout(timeoutId);
-        };
-
-        const listener = (transactionData: ITransactionData): void => {
-          const confirm = (): void => {
-            try { observer.next?.(transactionData); } catch (error) { try { observer.error?.(error); } catch {} }
-            complete();
-          };
-
-          if(txId === transactionData.id)
-            confirm();
-        };
-        this.worker.on("transaction", listener);
-
-        if(typeof expireIn === "number")
-          timeoutId = setTimeout(() => {
-            try { observer.error?.(new WorkerBeeError("Transaction expired")); } catch {}
-
-            complete();
-          }, expireIn);
-
-        return {
-          unsubscribe: (): void => {
-            complete();
-          }
-        };
-      }
-    };
-  }
-
-  public accountOperations(name: string): Subscribable<IOperationData> {
-    return {
-      subscribe: (observer: Partial<Observer<IOperationData>>): Unsubscribable => {
-        const complete = (): void => {
-          try { observer.complete?.(); } catch (error) { try { observer.error?.(error); } catch {} }
-          this.worker.off("transaction", listener);
-        };
-
-        const visitor = new AccountOperationVisitor(name);
-
-        const listener = (transactionData: ITransactionData): void => {
-          const confirm = (result: operation): void => {
-            try { observer.next?.({ op: result, transaction: transactionData }); } catch (error) { try { observer.error?.(error); } catch {} }
-          };
-
-          const proto = this.worker.chain!.TransactionBuilder.fromApi(transactionData.transaction).build();
-
-          for(const op of proto.operations) {
-            const result = visitor.accept(op);
-
-            if(typeof result === "object")
-              confirm(result);
-          }
-        };
-        this.worker.on("transaction", listener);
-
-        return {
-          unsubscribe: (): void => {
-            complete();
-          }
-        };
-      }
-    };
-  }
-}
 
 export class WorkerBee extends EventEmitter implements IWorkerBee {
   public running: boolean = false;
---
+++ src/queen.ts
@@ -0,0 +1,117 @@
+import type { operation } from "@hive-staging/wax";
+import type { Subscribable, Observer, Unsubscribable } from "rxjs";
+
+import { AccountOperationVisitor } from "./account_observer";
+import type { WorkerBee } from "./bot";
+import { WorkerBeeError } from "./errors";
+import type { IBlockData, ITransactionData, IOperationData } from "./interfaces";
+
+export class QueenBee {
+  public constructor(
+    private readonly worker: WorkerBee
+  ) {}
+
+  public block(idOrNumber: string | number): Subscribable<IBlockData> {
+    return {
+      subscribe: (observer: Partial<Observer<IBlockData>>): Unsubscribable => {
+        const complete = (): void => {
+          try { observer.complete?.(); } catch (error) { try { observer.error?.(error); } catch {} }
+          this.worker.off("block", listener);
+        };
+
+        const listener = (blockData: IBlockData): void => {
+          const confirm = (): void => {
+            try { observer.next?.(blockData); } catch (error) { try { observer.error?.(error); } catch {} }
+            complete();
+          };
+
+          if(typeof idOrNumber === "string") {
+            if(idOrNumber === blockData.block.block_id)
+              confirm();
+          } else if(idOrNumber === blockData.number)
+            confirm();
+        };
+        this.worker.on("block", listener);
+
+        return {
+          unsubscribe: (): void => {
+            complete();
+          }
+        };
+      }
+    };
+  }
+
+  public transaction(txId: string, expireIn?: number): Subscribable<ITransactionData> {
+    return {
+      subscribe: (observer: Partial<Observer<ITransactionData>>): Unsubscribable => {
+        let timeoutId: undefined | NodeJS.Timeout = undefined;
+
+        const complete = (): void => {
+          try { observer.complete?.(); } catch (error) { try { observer.error?.(error); } catch {} }
+          this.worker.off("transaction", listener);
+          clearTimeout(timeoutId);
+        };
+
+        const listener = (transactionData: ITransactionData): void => {
+          const confirm = (): void => {
+            try { observer.next?.(transactionData); } catch (error) { try { observer.error?.(error); } catch {} }
+            complete();
+          };
+
+          if(txId === transactionData.id)
+            confirm();
+        };
+        this.worker.on("transaction", listener);
+
+        if(typeof expireIn === "number")
+          timeoutId = setTimeout(() => {
+            try { observer.error?.(new WorkerBeeError("Transaction expired")); } catch {}
+
+            complete();
+          }, expireIn);
+
+        return {
+          unsubscribe: (): void => {
+            complete();
+          }
+        };
+      }
+    };
+  }
+
+  public accountOperations(name: string): Subscribable<IOperationData> {
+    return {
+      subscribe: (observer: Partial<Observer<IOperationData>>): Unsubscribable => {
+        const complete = (): void => {
+          try { observer.complete?.(); } catch (error) { try { observer.error?.(error); } catch {} }
+          this.worker.off("transaction", listener);
+        };
+
+        const visitor = new AccountOperationVisitor(name);
+
+        const listener = (transactionData: ITransactionData): void => {
+          const confirm = (result: operation): void => {
+            try { observer.next?.({ op: result, transaction: transactionData }); } catch (error) { try { observer.error?.(error); } catch {} }
+          };
+
+          const proto = this.worker.chain!.TransactionBuilder.fromApi(transactionData.transaction).build();
+
+          for(const op of proto.operations) {
+            const result = visitor.accept(op);
+
+            if(typeof result === "object")
+              confirm(result);
+          }
+        };
+        this.worker.on("transaction", listener);
+
+        return {
+          unsubscribe: (): void => {
+            complete();
+          }
+        };
+      }
+    };
+  }
+}
