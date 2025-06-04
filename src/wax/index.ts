d3679ac3f5b9047905cc4321091cc4f68d3ce302
mtyszczak <mateusz.tyszczak@gmail.com>
2025-06-04 09:25:40 +0200

    Add typing for find_comments API

--- src/wax/index.ts
+++ src/wax/index.ts
@@ -1,4 +1,4 @@
-import { createHiveChain, IWaxOptionsChain, price, TWaxExtended } from "@hiveio/wax";
+import { asset, beneficiary_route_type, createHiveChain, IWaxOptionsChain, price, TAccountName, TWaxExtended } from "@hiveio/wax";
 
 export type WaxExtendTypes = {
   database_api: {
@@ -20,6 +20,44 @@ export type WaxExtendTypes = {
         running_version: string;
         last_confirmed_block_num: number;
         // ...
+      }>; };
+    };    find_comments: {
+      params: { comments: Array<[TAccountName, string]> };
+      result: { comments: Array<{
+        abs_rshares: 150255792948762,
+        allow_curation_rewards: boolean;
+        allow_replies: boolean;
+        allow_votes: boolean;
+        author: TAccountName;
+        author_rewards: number;
+        beneficiaries: Array<beneficiary_route_type>;
+        body: string;
+        cashout_time: string;
+        category: string
+        children: number;
+        children_abs_rshares: number;
+        created: string;
+        curator_payout_value: asset;
+        depth: number;
+        id: number;
+        json_metadata: string;
+        last_payout: string;
+        last_update: string;
+        max_accepted_payout: asset;
+        max_cashout_time: string;
+        net_rshares: number | string;
+        net_votes: number;
+        parent_author: string
+        parent_permlink: string;
+        percent_hbd: number;
+        permlink: string;
+        reward_weight: string;
+        root_author: TAccountName;
+        root_permlink: string;
+        title: string;
+        total_payout_value: asset;
+        total_vote_weight: number | string;
+        vote_rshares: number | string;
       }>; };
     };
     find_decline_voting_rights_requests: {
