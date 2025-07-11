314c1ea547f8af663ff651e4ab9063b130ed0a22
fwaszkiewicz <fabianwaszkiewicz09@gmail.com>
2025-07-11 11:07:26 +0200

    Run tests in multiple workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
+  fullyParallel: true,
   reporter: [
     [ "junit", { outputFile: "results.xml" } ],
     [ "json",  { outputFile: "results.json" } ]
