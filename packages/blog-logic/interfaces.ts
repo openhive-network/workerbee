df97cd1f166c116a22407975a9dc56b1c339f975
jlachor <jlachor@syncad.com>
2025-07-22 10:35:56 +0200

    Create alpha version of blog logic interfaces

---
+++ packages/blog-logic/interfaces.ts
@@ -0,0 +1,69 @@
+// WORK IN PROGRESS
+
+interface IVote {
+  weight: number;
+  upvote: boolean;
+  voter: string;
+}
+
+interface ICommunityShort {
+  id: number;
+  name: string;
+  title: string;
+}
+interface ICommunity extends ICommunityShort {
+  about: string;
+  admins: string[];
+  avatar_url: string;
+  creation_date: Date;
+  subscribers_count: number;
+  authors_count: number;
+  pending_count: number;
+}
+
+interface IBlogUser {
+  id: number;
+  creation_date: Date;
+  name: string;
+  comment_count: number;
+  post_count: number;
+  last_activity: Date;
+  profile_description: string;
+}
+
+interface IComment {
+  id: string;
+  author: IAuthorShort;
+  content: string;
+  published_at: Date;
+  updated_at: Date;
+  url: string;
+}
+
+interface ISession {
+
+}
+
+interface IAuthorShort {
+  name: string;
+  profile_url: string;
+  avatar: string;
+}
+
+interface IPostProperties {
+  id: string;
+  title: string;
+  slug: string;
+  author: IAuthorShort;
+  summary: string;
+  tags: string[];
+  community: ICommunityShort;
+  published_at: Date;
+  updated_at: Date;
+  url: string;
+}
+
+interface IPost extends IPostProperties {
+  content: string;
+}
+
