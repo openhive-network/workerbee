65b51b51382fa7432015b1e8dd735c28891b1b9b
jlachor <jlachor@syncad.com>
2025-09-15 12:19:33 +0200

    Properly use Blog Logic in rest API

--- examples/wordpress-rest-api/wordpress-rest-api.ts
+++ examples/wordpress-rest-api/wordpress-rest-api.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import { createHiveChain } from "@hiveio/wax";
+import { BlogPostOperation, createHiveChain } from "@hiveio/wax";
 import { Entry, ExtendedNodeApi } from "./hive";
-import { mapHiveCommentToWPComment, mapHivePostToWpPost, mapHiveTagsToWpTags } from "./hiveToWpMap";
+import { mapHiveCommentToWPComment, mapHivePostToWpPost, mapHiveTagsToWpTags, mapIPostToWpPost } from "./hiveToWpMap";
 import { WPComment, WPPost } from "./wp-reference";
 import { simpleHash } from "./hash-utils";
 import { wordPressExampleConfig } from "./example-config";
+import { IPost } from "../../packages/blog-logic/interfaces";
+import { BloggingPlaform } from "../../packages/blog-logic/BloggingPlatform";
 
 const hiveChain = await createHiveChain();
 const extendedHiveChain = hiveChain.extend<ExtendedNodeApi>();
@@ -19,6 +21,9 @@ app.use(cors());
 const apiRouter = express.Router();
 
 const idToStringMap = new Map<number, string>();
+
+const posts: IPost[] = [];
+const bloggingPlatform: BloggingPlaform = new BloggingPlaform();
 
 const getAuthorPermlinkFromSlug = (slug: string): {author: string, permlink: string} => {
   const splitedSlug = slug.split("_");
@@ -43,6 +48,18 @@ const mapAndAddPostsToMap = (posts: Entry[]): WPPost[] => {
 }
 
 
+const mapAndAddtoMapPosts = async (posts: IPost[]): Promise<WPPost[]> => { 
+  const mappedPosts: WPPost[] = [];
+  posts.forEach(async (post) => {
+    const postId = simpleHash(`${post.author.name}_${post.permlink}`);
+    const authorId = simpleHash(post.author.name);
+    idToStringMap.set(postId, `${post.author.name}_${post.permlink}`).set(authorId, post.author.name);
+    mappedPosts.push(await mapIPostToWpPost(post, postId, authorId));
+  });
+  return await mappedPosts;
+}
+
+
 apiRouter.get("/posts", async (req: Request, res: Response) => {
   // Default WP call for devtools
   if (req.query.slug === "com.chrome.devtools.json") res.json([]);
@@ -52,24 +69,27 @@ apiRouter.get("/posts", async (req: Request, res: Response) => {
     const authorPermlinkHash = simpleHash(req.query.slug);
     const authorHash = simpleHash(author);
     idToStringMap.set(authorPermlinkHash, req.query.slug as string).set(authorHash, author);
+    const post = await bloggingPlatform.getPost({author: {name: author}, permlink});
     const result = await extendedHiveChain.api.bridge.get_post({author, permlink, observer: "hive.blog"});
-    if (result) {
-      res.json(mapHivePostToWpPost(result, authorPermlinkHash, authorHash));
+    if (post) {
+      res.json(await mapIPostToWpPost(post, authorPermlinkHash, authorHash));
     } else {
       res.status(404).json({ error: "Post not found" });
     }
     // Posts list
   } else {
-    const result = await extendedHiveChain.api.bridge.get_ranked_posts({
+    const posts = await bloggingPlatform.enumPosts({
       limit: wordPressExampleConfig.postLimit, 
       sort: wordPressExampleConfig.sort, 
-      observer: wordPressExampleConfig.observer, 
-      start_author: wordPressExampleConfig.startAuthor, 
-      start_permlink: wordPressExampleConfig.startPermlink, 
+      startAuthor: wordPressExampleConfig.startAuthor, 
+      startPermlink: wordPressExampleConfig.startPermlink, 
       tag: wordPressExampleConfig.postTag
-    });
-    if (result) {
-      res.json(mapAndAddPostsToMap(result));
+    }, {
+      page: 1,
+      pageSize: 10
+    }) as IPost[];
+    if (posts) {
+      res.json(await mapAndAddtoMapPosts(posts));
     }
   }
 
