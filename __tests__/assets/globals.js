6a769171d0256ca98592f05770785ee365bd230e
mtyszczak <mateusz.tyszczak@gmail.com>
2025-11-05 13:43:17 +0100

    Bump dependencies

--- __tests__/assets/globals.js
+++ __tests__/assets/globals.js
@@ -1,13 +1,11 @@
 // Use function as we later extract the function name in the jest-helpers
 globalThis.createTestFor = async function createTestFor(env) {
   const locWorkerBee = env === "web" ? "../../dist/bundle/index.js" : "../../dist/bundle/index.js";
-  const locBeekeeper = env === "web" ? "@hiveio/beekeeper/web" : "@hiveio/beekeeper/node";
-  const locWax = env === "web" ? "@hiveio/wax/web" : "@hiveio/wax/node";
 
   // Import required libraries env-dependent
   const wb = await import(locWorkerBee);
-  const beekeeper = await import(locBeekeeper);
-  const wax = await import(locWax);
+  const beekeeper = await import("@hiveio/beekeeper");
+  const wax = await import("@hiveio/wax");
 
   // Provide results
   return {
